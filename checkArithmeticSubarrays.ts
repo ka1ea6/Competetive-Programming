27f84b44bfa2e693e47d75879807c69d1ccd2495
ka1ea6 <kaleab77gulilat@gmail.com>
2022-05-24 10:26:31 +0300

    some changes on check arithmetic sub arrays

--- checkArithmeticSubarrays.ts
+++ checkArithmeticSubarrays.ts
@@ -6,7 +6,6 @@ function checkArithmeticSubarrays(
   const answers: boolean[] = [];
   for (let i = 0; i < l.length; i++) {
     let currArr = [...nums].slice(l[i], r[i] + 1).sort((a, b) => a - b);
-    console.log(currArr);
     if (currArr.length === 1) {
       answers.push(false);
       continue;
@@ -24,11 +23,3 @@ function checkArithmeticSubarrays(
   }
   return answers;
 }
-// console.log(checkArithmeticSubarrays([4, 6, 5, 9, 3, 7], [0, 0, 2], [2, 3, 5]));
-console.log(
-  checkArithmeticSubarrays(
-    [-12, -9, -3, -12, -6, 15, 20, -25, -20, -15, -10],
-    [0, 1, 6, 4, 8, 7],
-    [4, 4, 9, 7, 9, 10]
-  )
-);
