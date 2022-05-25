61a73d71c3f37e53abbe4f52a07cc709ad7b66b4
ka1ea6 <kaleab77gulilat@gmail.com>
2022-05-25 13:03:28 +0300

    queue implementation using array

---
+++ myQueue.ts
@@ -0,0 +1,29 @@
+class MyQueue {
+  arr: number[] = [];
+  constructor() {}
+
+  push(x: number): void {
+    this.arr.push(x);
+  }
+
+  pop(): number {
+    return this.arr.shift();
+  }
+
+  peek(): number {
+    return this.arr[0];
+  }
+
+  empty(): boolean {
+    return this.arr.length === 0;
+  }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
